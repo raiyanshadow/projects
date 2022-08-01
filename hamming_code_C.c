842c1ec7c53f0bc9a09e1ad204f751104862f5e9
raiyan <67802404+raiyanshadow@users.noreply.github.com>
2022-08-01 00:42:58 -0400

    made it better

--- hamming_code_C.c
+++ hamming_code_C.c
@@ -4,29 +4,46 @@
 
 #include <stdio.h>
 #include <math.h>
-#define MAXLEN 4                // you change this depending on the largest digit of the corresponding binary number, in this example 1110 is the biggest binary digit, hence MAXLEN is 4
+#include <stdlib.h>
+#define TYPE 32
+
+int MAXLEN = log10(TYPE) / log10(2);
 
 long int binConvert(long int a);
-int *binBag(long int a);
+int binBag(long int a, int index);
 int numLen(long int a);
-int revBinConvert(int *a);
-int binArrayNumLen(int *a);
+int revBinConvert(int* a);
+int binArrayNumLen(int* a);
 
 int main()
 {
-    int code[16] = {0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0};           // change this to your liking, or make it dynamic :)
+    int code[TYPE] = { 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 }; // change this to your liking, or make it dynamic :)
     int count = 0;
-    
-    for (int i = 0; i < 16; i++)
+
+    for (int i = 0; i < TYPE; i++)
     {
         count += (code[i] == 1 ? 1 : 0);
     }
-    
-    long int preBin[count];
-    long int postBin[count];
-    int finBin[count][MAXLEN];
-    
-    for (int i = 0, j = 0; i < 16; i++)
+
+    if (count % 2 == 0)
+    {
+        printf("No errors\n");
+        return -1;
+    }
+    else
+    {
+        
+    }
+
+    long int *preBin = malloc(count * sizeof(long int));
+    long int *postBin = malloc(count * sizeof(long int));
+    int** finbin = malloc(count * sizeof(int*));
+    for (int i = 0; i < count; i++)
+    {
+        finbin[i] = malloc(MAXLEN * sizeof(int));
+    }
+
+    for (int i = 0, j = 0; i < TYPE; i++)
     {
         if (code[i] == 1)
         {
@@ -34,50 +51,54 @@ int main()
             j++;
         }
     }
-    
+
     for (int i = 0; i < count; i++)
     {
         postBin[i] = binConvert(preBin[i]);
         printf("%li\n", postBin[i]);
     }
     printf("\n");
-    
+
+    int boolean = 0;
+    int initialize = 0;
+
     for (int i = 0; i < count; i++)
     {
         for (int j = 0; j < MAXLEN; j++)
-        {
-            finBin[i][j] = binBag(postBin[i])[j];
-            printf("%i, ", binBag(postBin[i])[j]);
+        {  
+            finbin[i][j] = binBag(postBin[i], j);
+            printf("%i, ", finbin[i][j]);
         }
         printf("\n");
     }
-    
-    long int temp = 0;
-    long int actemp = 0;
-    int mode = 0;
-    int finum[MAXLEN];
+
+    int *finum = malloc(MAXLEN * sizeof(int));
     int finfinum = 0;
-    
-    for (int i = 0; i <= MAXLEN; i++, mode++)
+
+    for (int i = 0; i < MAXLEN; i++)
     {
         for (int j = 0; j < count; j++)
         {
-            finum[i] += finBin[j][i];
+            finum[i] += finbin[j][i];
         }
         finum[i] %= 2;
     }
-    
-    for (int i = MAXLEN - binArrayNumLen(finum) - 1, k = MAXLEN-1; i < MAXLEN; i++, k--)
+
+    for (int i = MAXLEN - binArrayNumLen(finum), k = MAXLEN - 1; i < MAXLEN; i++, k--)
     {
         finfinum += finum[i] * pow(10, k);
     }
-    
+
     for (int i = 0; i < MAXLEN; i++)
     {
         printf("= %i\n", finum[i]);
     }
-    
-    printf("Error found at 0x%i (Position %i)", binConvert(revBinConvert(finum)), revBinConvert(finum));    // lol, i dont know why the error address is always 0x100 if i just use finum, oh well
+
+    printf("Error found at 0x%li (Position %i)", finfinum, revBinConvert(finum));
+    free(finum);
+    free(finbin);
+    free(postBin);
+    free(preBin);
     return 0;
 }
 
@@ -87,7 +108,7 @@ long int binConvert(long int a)
     int binLen = floor(roughBinLen);
     long int bin = (a % 2 == 1 ? 1 : 0);
     a = floor(a / 2);
-    
+
     for (int i = 1; i <= binLen; i++, a = floor(a / 2))
     {
         bin += (a % 2 == 1 ? pow(10, i) : 0);
@@ -95,20 +116,20 @@ long int binConvert(long int a)
     return bin;
 }
 
-int *binBag(long int a)
+int binBag(long int a, int index)
 {
-    int len = numLen(a) - 1;
     int offset = MAXLEN - numLen(a);
     long int s1 = a;
     int s2 = 0;
-    static int num[MAXLEN];
-    for (int i = len; i >= 0; i--, s2++)
+    int* num = malloc(MAXLEN * sizeof(int));
+ 
+    for (int i = MAXLEN - 1; i >= 0; i--, s2++)
     {
         if (a < pow(10, i))
         {
             num[s2] = 0;
         }
-        else if (s1 < pow(10, MAXLEN-1))
+        else if (s1 < pow(10, MAXLEN - 1))
         {
             num[i + offset] = a % 10;
             a /= 10;
@@ -117,9 +138,11 @@ int *binBag(long int a)
         {
             num[i] = a % 10;
             a /= 10;
-        }    
+        }
     }
-    return num;
+    int finalNum = num[index];
+    free(num);
+    return finalNum;
 }
 
 int numLen(long int a)
@@ -136,19 +159,19 @@ int numLen(long int a)
     return c;
 }
 
-int revBinConvert(int *a)
+int revBinConvert(int* a)
 {
     int actual = 0;
-    
-    for (int i = 0, j = MAXLEN-1; i < MAXLEN; i++, j--)
+
+    for (int i = 0, j = MAXLEN - 1; i < MAXLEN; i++, j--)
     {
         actual += (a[i] == 1 ? pow(2, j) : 0);
     }
-    
+
     return actual;
 }
 
-int binArrayNumLen(int *a)
+int binArrayNumLen(int* a)
 {
     int pos = 0;
     int haveseen = 0;
@@ -166,6 +189,6 @@ int binArrayNumLen(int *a)
             i++;
         }
     }
-    
+
     return MAXLEN - pos;
 }
